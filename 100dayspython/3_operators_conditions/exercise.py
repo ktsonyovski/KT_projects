d975d2a8a9093cb8857ea0267e4dac95a80fee6e
ktsonyovski <ktsonyovski@outlook.com>
2025-01-03 00:00:39 +0200

    day_4

---
+++ 100dayspython/3_operators_conditions/exercise.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------
+# # check if number is even
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("Number is even!")
+# else:
+#     print("Number is odd!")
+
+# ---------------------------------------------
+# weight = 85
+# height = 1.85
+
+# bmi = weight / (height ** 2)
+
+# # 🚨 Do not modify the values above
+# # Write your code below 👇
+
+# if bmi < 18.5:
+#     print("underweight")
+# elif 18.5 <= bmi < 25:
+#     print("normal weight")
+# else:
+#     print("overweight")
---
+++ 100dayspython/4_lists/exercise.py
@@ -0,0 +1,13 @@
+import random
+options_computer = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options_computer)
+user_choice = str(input("Welcome to the rock, paper, scissors game. \nPlease provide your choice: ")).lower()
+
+if user_choice == computer_choice:
+    print(f"You chose {user_choice}.\nThe computer chose {computer_choice}\nIt's a draw!")
+elif (user_choice == "rock" and computer_choice == "paper") or (user_choice == "scissors" and computer_choice == "rock") or (user_choice == "paper" and computer_choice == "scissors"):
+    print(f"You chose {user_choice}.\nThe computer chose {computer_choice}\nYou lose!")
+elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "scissors" and computer_choice == "paper") or (user_choice == "paper" and computer_choice == "rock"):
+    print(f"You chose {user_choice}.\nThe computer chose {computer_choice}\nYou win!")        
+else:
+    print("Incorrect input, try again!")
