f13d27118afbf9935cdc663c7bbe45d149e565a7
ktsonyovski <ktsonyovski@outlook.com>
2025-01-12 23:56:16 +0200

    day_5

---
+++ 100dayspython/5_day/exec.py
@@ -0,0 +1,24 @@
+# scores = [1001, 2, 124, 165, 173, 189, 169, 146, 2]
+# min_score = scores[0]
+# for item in scores:
+#     if item < min_score:
+#         min_score = item
+#         print(min_score)
+# print(min_score)
+
+# gauss
+# sum = 0
+# for number in range(1, 101):
+#     sum += number
+# print(sum)    
+
+# fizz_buzz 
+# for i in range(1, 101):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     else:
+#         print(i)
