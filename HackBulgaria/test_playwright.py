5966df177d4be49c77cf521225699dd21fd01d26
ktsonyovski <ktsonyovski@DESKTOP-DHNBTRE.>
2025-01-26 22:36:17 +0200

    HackBulgaria

---
+++ HackBulgaria/test_playwright.py
@@ -0,0 +1,12 @@
+from resources import *
+
+def test_login_success(page):
+    login_success(page)
+
+def test_add_user_success(page):
+    login_success(page)
+    user_add(page)
+    
+def test_delete_all_users_success(page):
+    login_success(page)
+    delete_all_users(page)
