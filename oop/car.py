ef41e81271d7b95be9818572b7276fb90ade68c9
ktsonyovski <ktsonyovski@outlook.com>
2025-01-28 23:17:57 +0200

    update_oop

--- oop/car.py
+++ oop/car.py
@@ -7,4 +7,12 @@ class car_make_model:
 
     def what_am_i(self):
         print(f"This is a {self.make}. It is a {self.year} year {self.model} in {self.color} colour!")
-    # 
+
+class engine:
+    def __init__(self,volume: int,cylinders,fuel: str):
+        self.volume = volume
+        self.cylinders = cylinders
+        self.fuel = fuel
+
+    def powertrain(self):
+        print(f"The power train includes a {self.volume} liter, {self.cylinders} cylinder {self.fuel} engine!")
--- oop/car_main.py
+++ oop/car_main.py
@@ -1,7 +1,8 @@
 import car
 
 best_car = car.car_make_model("BMW", "E36", 1991, "black pearl")
+best_car_engine = car.engine(1991, 6, "Petrol")
 the_most_reliable_car = car.car_make_model("Mazda", "RX-8", 2010, "bright red")
 
 best_car.what_am_i()
-the_most_reliable_car.what_am_i()
+best_car_engine.powertrain()
